6c30e9fca246753f9ad37e7c68e2604258e2021f
onsattia <onesattia@gmail.com>
2018-10-15 20:12:30 +0100

    added file delete func

--- app.js
+++ app.js
@@ -7,5 +7,12 @@ var readFile = fs.readFileSync('index.html', 'utf8');
 fs.appendFileSync('index.html', "\n<p>It Works</p>");
 
 // Create an empty file 
-fs.closeSync(fs.openSync('test.txt', 'w'));
+// fs.closeSync(fs.openSync('test.txt', 'w'));
 
+// Delete file
+try {
+    fs.unlinkSync('test.txt');
+    console.log('file successfully deleted');
+  } catch (err) {
+    console.log(err);
+}
